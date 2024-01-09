60cd86cace065c720e3d71d7da4664c2e08326c9
tkbro <wlehoapt@gmail.com>
2024-01-10 01:18:00 +0900

    Add imageGenerate API and setting config

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+// 환경 변수를 읽어옵니다. 값이 없다면 'local'로 설정합니다.
+const path = require("path");
+const environment = process.env.NODE_ENV || "local";
+const configPath = path.resolve(__dirname, "config", environment);
+process.env["NODE_CONFIG_DIR"] = configPath;
+console.log("env" + process.env.NODE_CONFIG_DIR);
+const config = require("config");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+// // 환경에 따라 설정 파일을 로드합니다.
+console.log("config:", config.util.toObject());
+app.use(bodyParser.json());
+
+app.post("/generateImage/:prompt", async (req, res) => {
+  try {
+    const prompt = req.params.prompt;
+
+    // prompt와 inputString을 사용하여 이미지를 생성하는 함수 호출
+    const generatedImage = await generateImage(prompt);
+
+    // 생성된 이미지를 클라이언트에 응답
+    res.json({ generatedImage });
+  } catch (error) {
+    console.error("Error generating image:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+// 이미지 생성 함수
+async function generateImage(prompt) {
+  // config.js에서 apiUrl 가져오기
+  const apiUrl = config.get("apiUrl");
+  const negativePrompt = config.get("negativePrompt");
+  const promptPrefix = config.get("promptPrefix");
+  const generateImageCount = 2;
+
+  // POST 요청 헤더 설정
+  const headers = {
+    Authorization: config.get("karloRestApiKey"),
+    "Content-Type": "application/json",
+    // 추가적인 헤더 값들을 설정할 수 있습니다.
+  };
+
+  const data = {
+    prompt: promptPrefix + prompt,
+    negative_prompt: negativePrompt,
+    samples: generateImageCount,
+    // 추가적인 필요한 데이터를 설정할 수 있습니다.
+  };
+
+  const response = await axios.post(apiUrl, data, { headers });
+
+  // 생성된 이미지 또는 이미지 URL을 반환
+  return response.data;
+}
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
---
+++ config/default/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiUrl: "",
+  karloRestApiKey: "",
+  negativePrompt: "",
+  promptPrefix: "",
+};
+
+// default 의 스키마를 따라서 local 폴더에 config 를 설정합니다.
